7d1b60dacd36d9992796f3d6ee1e4dedb0ed6772
WYJ <2447206328@qq.com>
2024-03-08 10:49:08 +0800

    Changes
    Signed-off-by: WYJ <2447206328@qq.com>

--- database.py
+++ database.py
@@ -4,8 +4,12 @@ import pandas as pd
 class ChatLogs:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.df = pd.read_csv(file_path)
-        print(self.df)
+        try:
+            self.df = pd.read_csv(file_path)
+            print(self.df)
+        except pd.errors.EmptyDataError or FileNotFoundError:
+            self.df = pd.DataFrame(columns=['Sender_ID', 'Receiver_ID', 'Message'])
+            self.df.to_csv(file_path, index=False)
 
     def insert_message(self, sender_id, receiver_id, message):
         new_row = {'Sender_ID': sender_id, 'Receiver_ID': receiver_id, 'Message': message}
@@ -23,8 +27,12 @@ class ChatLogs:
 class User_info:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.df = pd.read_csv(file_path)
-        print(self.df)
+        try:
+            self.df = pd.read_csv(file_path)
+            print(self.df)
+        except pd.errors.EmptyDataError or FileNotFoundError:
+            self.df = pd.DataFrame(columns=['User_ID', 'User_Name', 'Password'])
+            self.df.to_csv(file_path, index=False)
 
     def insert_user(self, user_id, user_name, password):
         new_row = {'User_ID': user_id, 'User_Name': user_name, 'Password': password}
@@ -43,22 +51,72 @@ class User_info:
         self.df.to_csv(self.file_path, index=False)
 
 
+class Friend_list:
+    def __init__(self, file_path):
+        self.file_path = file_path
+        try:
+            self.df = pd.read_csv(file_path)
+        except pd.errors.EmptyDataError or FileNotFoundError:
+            self.df = pd.DataFrame(columns=['User_ID', 'Friend_ID'])
+            self.df.to_csv(file_path, index=False)
+    def insert_friend(self, user_id, friend_id):
+        user_id = eval(user_id)
+        new_row = {'User_ID': user_id, 'Friend_ID': friend_id}
+        self.df = pd.concat([self.df, pd.DataFrame([new_row])], ignore_index=True, sort=False)
+        self.df.to_csv(self.file_path, index=False)
+
+    def get_friends(self, user_id):
+        user_id = eval(user_id)
+        friends = self.df[self.df['User_ID'] == user_id]
+        return friends["Friend_ID"].tolist()
+
+
+    def delete_friend(self, user_id, friend_id):
+        user_id = eval(user_id)
+        self.df = self.df[(self.df['User_ID'] != user_id) | (self.df['Friend_ID']!= friend_id)]
+        self.df.to_csv(self.file_path, index=False)
+
+    def close_connection(self):
+        self.df.to_csv(self.file_path, index=False)
+
+
+class Chat_logs:
+
+    def close_connection(self):
+        self.df.to_csv(self.file_path, index=False)
+
+
+
+
 if __name__ == '__main__':
-    chat_logs = ChatLogs('chat_logs.csv')
-    chat_logs.insert_message(2, 1, 'Hi')
-    chat_logs.insert_message(1, 2, 'How are you?')
-    chat_logs.insert_message(2, 1, 'I am good, thank you!')
-
-    messages = chat_logs.get_messages(6, 2)
-    print(messages)
-
-    chat_logs.close_connection()
-
-    log_in = User_info('user_info.csv')
-    # log_in.insert_user(1, 'user1', 'password1')
-    # log_in.insert_user(2, 'user2', 'password2')
-    # log_in.insert_user(3, 'user3', 'password3')
-
-    user = log_in.get_user(1, 'password1')
-    print(user)
-    log_in.close_connection()
+    # chat_logs = ChatLogs('chat_logs.csv')
+    # chat_logs.insert_message(2, 1, 'Hi')
+    # chat_logs.insert_message(1, 2, 'How are you?')
+    # chat_logs.insert_message(2, 1, 'I am good, thank you!')
+    #
+    # messages = chat_logs.get_messages(6, 2)
+    # print(messages)
+    #
+    # chat_logs.close_connection()
+    #
+    # log_in = User_info('user_info.csv')
+    # # log_in.insert_user(1, 'user1', 'password1')
+    # # log_in.insert_user(2, 'user2', 'password2')
+    # # log_in.insert_user(3, 'user3', 'password3')
+    #
+    # user = log_in.get_user(1, 'password1')
+    # print(user)
+    # log_in.close_connection()
+    friends = Friend_list('friend_list.csv')
+    friends.insert_friend("1","sam")
+    friends.insert_friend("1","sam")
+    friends.insert_friend("2","john")
+    friends.insert_friend("3","tom")
+    friends.insert_friend("4","jerry")
+    friends.insert_friend("1","jim")
+    friends.insert_friend("1","Daming")
+    friend = friends.get_friends("1")
+    print(friend)
+    friends.delete_friend("1","sam")
+    friend = friends.get_friends("1")
+    print(friend)
